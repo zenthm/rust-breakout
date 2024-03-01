056739c82fb71f1755e5d4550a74c2c872b177e2
Zenthm <zenthm.dev@gmail.com>
2024-03-01 10:56:24 +0800

    Update codebase

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("breakout")]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+        next_frame().await
+    }
 }
